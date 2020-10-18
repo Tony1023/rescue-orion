01de071133098f6bcd08f666d4c350398c39edb0
Zhehao /Tony/ Lu <tony1023lv@gmail.com>
2020-10-18 20:29:16 +0800

    Printing the states

--- client/src/admin/RoomListPage.tsx
+++ client/src/admin/RoomListPage.tsx
@@ -1,12 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import SocketIOClient from 'socket.io-client';
-import { LobbyUpdate } from '../metadata/types';
+import { LobbyState, LobbyUpdate } from '../metadata/types';
 
 export default () => {
   const { code } = useParams<{ code?: string }>();
 
   const [socket, setSocket] = useState<SocketIOClient.Socket>();
+  const [lobbyState, setLobbyState] = useState<string>();
 
   useEffect(() => {
     const newSocket = SocketIOClient('http://localhost:9000', {
@@ -17,7 +18,8 @@ export default () => {
     });
 
     newSocket.on(LobbyUpdate, (data: string) => {
-      console.log(JSON.parse(data));
+      const state = JSON.parse(data) as LobbyState;
+      setLobbyState(JSON.stringify(state, undefined, 2));
     });
 
     newSocket.on('disconnect', () => {
@@ -30,6 +32,8 @@ export default () => {
   }, [code]);
 
   return <>
-
+    <pre>
+      {lobbyState}
+    </pre>
   </>
 }
