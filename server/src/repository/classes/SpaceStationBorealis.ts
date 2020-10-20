2bf1058556aa13a3a8a86d1df400ee5c3ba922b4
Vivian Zhu <tongyu@usc.edu>
2020-10-20 13:44:03 +0800

    add checkDropOff function prototype and complete Boreallis dropoff message

--- server/src/repository/classes/SpaceStationBorealis.ts
+++ server/src/repository/classes/SpaceStationBorealis.ts
@@ -20,4 +20,18 @@ export default class SpaceStationBorealis extends SpaceStation {
     return false;
   }
 
+  checkDropOffResource(r: RescueResource): void {
+    if(r === RescueResource.AITechnology) {
+      this.messageQueue.pushMessage({
+        title: 'AI TECHNOLOGY DELIVERED',
+          paragraphs: [
+            { text: 'Well done!' },
+            { text: 'With the AI Technology from Capricorn here to replace them, the Oxygen Repair Team can now leave Borealis safely!' },
+            { text: 'The Oxygen Repair Team are ready to board your ship!' },
+            { text: '-Ground Control' },
+          ],
+      })
+    }
+  }
+
 }
