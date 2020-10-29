0378b8b504f6c5ffd0326adf54ec9d0dde7c7c9c
Zhehao /Tony/ Lu <tony1023lv@gmail.com>
2020-10-29 22:39:44 +0800

    2-space indent

--- client/src/admin/LobbyListPage.tsx
+++ client/src/admin/LobbyListPage.tsx
@@ -4,45 +4,45 @@ import { Jumbotron, Table, Button, Badge, Modal } from 'react-bootstrap';
 
 export default () => {
 
-    const [lobbies, setLobbies] = useState<{ code: number, createTime: number }[]>([]);
+  const [lobbies, setLobbies] = useState<{ code: number, createTime: number }[]>([]);
 
-    useEffect(() => {
-        client.get('http://localhost:9000/lobbies',
-            { headers: { Authorization: `bearer ${localStorage.getItem('token')}` }}).then((res) => {
-            setLobbies(res.data);
-        });
-    },[]);
+  useEffect(() => {
+    client.get('http://localhost:9000/lobbies',
+      { headers: { Authorization: `bearer ${localStorage.getItem('token')}` }}).then((res) => {
+      setLobbies(res.data);
+    });
+  },[]);
 
-    async function post() {
-        let res = await client.post('http://localhost:9000/lobbies', {}, { headers: { Authorization: `bearer ${localStorage.getItem('token')}` }});
-    }
+  async function post() {
+    let res = await client.post('http://localhost:9000/lobbies', {}, { headers: { Authorization: `bearer ${localStorage.getItem('token')}` }});
+  }
+  
+  return <>
+  {
     
-    return <>
-    {
-        
-        <div>
-         <button onClick={post}>Create Lobby</button>
-            <div>
-            <Table striped bordered hover variant="dark">
-            <thead>
-                <tr>
-                    <th>Lobby Code</th>
-                    <th>Create Time</th>
-                </tr>
-            </thead>
-            <tbody id="lobbies">
-                {
-                    lobbies.map((lobby) => <tr>
-                        <td>{lobby.code}</td>
-                        <td>{lobby.createTime}</td>
-                    </tr>)
-                }
-            </tbody>
-            </Table>
-            </div>
-            {/* <div>{lobbyCodes}</div>
-            <div>{lobbyTimes}</div> */}
-        </div>
-    }
-    </>
+    <div>
+     <button onClick={post}>Create Lobby</button>
+      <div>
+      <Table striped bordered hover variant="dark">
+      <thead>
+        <tr>
+          <th>Lobby Code</th>
+          <th>Create Time</th>
+        </tr>
+      </thead>
+      <tbody id="lobbies">
+        {
+          lobbies.map((lobby) => <tr>
+            <td>{lobby.code}</td>
+            <td>{lobby.createTime}</td>
+          </tr>)
+        }
+      </tbody>
+      </Table>
+      </div>
+      {/* <div>{lobbyCodes}</div>
+      <div>{lobbyTimes}</div> */}
+    </div>
+  }
+  </>
 }
