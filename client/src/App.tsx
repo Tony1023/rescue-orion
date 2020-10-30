efdea54a8d4014258f361f9e39c9c1cfc4f636c1
Zhehao /Tony/ Lu <tony1023lv@gmail.com>
2020-10-30 18:46:17 +0800

    Remove dev deps

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
-import Room, { DevRoom } from './room';
+import Room from './room';
 import Admin from './admin';
 import Join from './join';
 
@@ -21,11 +21,6 @@ export default () => {
         path='/join'
         component={Join}
       />
-      <Route
-        path='/dev'
-        component={DevRoom}
-      />
-      {/* <Route component={allotherlinks} /> */}
     </Switch>
   </BrowserRouter>
 }
--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -22,33 +22,4 @@ export default (app: express.Express, server: http.Server): void => {
   const roomRouter = express.Router();
   app.use('/rooms', roomRouter);
   room(roomRouter, roomWss);
-
-  /** Dev only stuff below, will be removed close to the end */
-  const devWss = io().path('/dev').attach(server);
-  devRoute(devWss);
 };
-
-
-import { Lobby, Room } from '../repository';
-import * as Types from '../metadata/types';
-
-function devRoute(wss: io.Server) {
-  const lobby = new Lobby(12306, 'admin');
-  if (!lobby.isRoomNameTaken('room')) {
-    lobby.insertRoom('room');
-  }
-  const room = lobby.findRoom('room');
-  wss.on('connection', (socket) => {
-    lobby.startGames();
-    room.setSocketAndPushUpdate(socket);
-
-    socket.on(Types.SocketMessages.Action, (json) => {
-      const action = JSON.parse(json.toString());
-      room.applyGameAction(action);
-    })
-
-    socket.on('disconnect', () => {
-      room.onSocketDisconnect();
-    });
-  });
-}
