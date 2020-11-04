794b8b599d018b19c8dd092cce3a0671e9dc61fc
Vivian Zhu <tongyu@usc.edu>
2020-11-04 15:18:57 +0800

    add confirm move helper function to replicate steps

--- client/src/__tests__/ResourcePanel.test.tsx
+++ client/src/__tests__/ResourcePanel.test.tsx
@@ -17,6 +17,36 @@ describe('Load resource panel and updates with move', () => {
   let room: ReactWrapper;
   let gameBoard: ReactWrapper;
 
+  const confirmMove = async(moveLocation: string, previousEnergy: string) => {
+    // press next move to moved location
+    const moveButtons = gameBoard.find(`[data-testid="move-${moveLocation}"]`);
+    expect(moveButtons.find(`div[data-testid="move-${moveLocation}-gemini12"]`)).toHaveLength(1);
+    moveButtons.find(`div[data-testid="move-${moveLocation}-gemini12"]`).simulate('click');
+    // open confirm move dialog
+    gameBoard.find('div[data-testid="confirm-move-dialog"]').simulate('click');
+    await act(async () => {
+      await waitFor(() => {
+        room.update();
+        gameBoard = room.find(GameBoard);
+        // should render confirm move modal
+        expect(gameBoard.find(ConfirmMoveModal)).toHaveLength(1)
+      });
+    });
+  
+    // confirm move
+    expect(gameBoard.find('div[data-testid="confirm-move-button"]')).toHaveLength(1);
+    gameBoard.find('div[data-testid="confirm-move-button"]').simulate('click');
+    await act(async () => {
+      await waitFor(() => {
+        room.update();
+        gameBoard = room.find(GameBoard);
+        // confirm modal should disappear and resource panel updated
+        expect(gameBoard.find(ConfirmMoveModal)).toHaveLength(0);
+        expect(gameBoard.find(ResourcePanel).find('div[data-testid="energy-gemini1"]').text().includes(previousEnergy)).toBeFalsy();
+      });
+    });
+  }
+
   beforeAll(async (done) => {
     // admin log in
     const res = await axios.post(`${API_BASE_URL}/admin/login`,
@@ -74,41 +104,49 @@ describe('Load resource panel and updates with move', () => {
     done();
   });
 
-  it('should reduce correct after a move', async (done) => {
+  it('should reduce correctly after a starway move', async (done) => {
     // press next move to Beacon Star 3
-    const moveButtons = gameBoard.find('[data-testid="move-b3"]');
-    expect(moveButtons.find('div[data-testid="move-b3-gemini12"]')).toHaveLength(1);
-    moveButtons.find('div[data-testid="move-b3-gemini12"]').simulate('click');
-    // open confirm move dialog
-    gameBoard.find('div[data-testid="confirm-move-dialog"]').simulate('click');
-    await act(async () => {
-      await waitFor(() => {
-        room.update();
-        gameBoard = room.find(GameBoard);
-        // should render confirm move modal
-        expect(gameBoard.find(ConfirmMoveModal)).toHaveLength(1)
-      });
-    });
+    await confirmMove('b3', '40');
 
-    // confirm move
-    expect(gameBoard.find('div[data-testid="confirm-move-button"]')).toHaveLength(1);
-    gameBoard.find('div[data-testid="confirm-move-button"]').simulate('click');
-    await act(async () => {
-      await waitFor(() => {
-        room.update();
-        gameBoard = room.find(GameBoard);
-        // confirm modal should disappear and resource panel updated
-        expect(gameBoard.find(ConfirmMoveModal)).toHaveLength(0);
-        expect(gameBoard.find(ResourcePanel).find('div[data-testid="energy-gemini1"]').text().includes('40')).toBeFalsy();
-      });
-    });
-
-    // check resource panel initial resource level
+    // check resource panel resource level
     gameBoard.update();
     const resourcePanel = gameBoard.find(ResourcePanel);
     // gemini 1
     expect(resourcePanel.find('div[data-testid="energy-gemini1"]').text()).toEqual('39');
     expect(resourcePanel.find('div[data-testid="lifeSupport-gemini1"]').text()).toEqual('79');
+    expect(resourcePanel.find('div[data-testid="resource-gemini1"]').text().includes('O2 Replacement Cells')).toBeTruthy();
+    // gemini 2
+    expect(resourcePanel.find('div[data-testid="energy-gemini2"]').text()).toEqual('40');
+    expect(resourcePanel.find('div[data-testid="lifeSupport-gemini2"]').text()).toEqual('100');
+    expect(resourcePanel.find('div[data-testid="resource-gemini2"]').text()).toEqual('');
+    done();
+  });
+
+  it('should reduce correctly after a hypergate move', async (done) => {
+    // press next move to Hypergate 1
+    await confirmMove('h1', '39');
+
+    // check resource panel resource level
+    gameBoard.update();
+    let resourcePanel = gameBoard.find(ResourcePanel);
+    // gemini 1
+    expect(resourcePanel.find('div[data-testid="energy-gemini1"]').text()).toEqual('38');
+    expect(resourcePanel.find('div[data-testid="lifeSupport-gemini1"]').text()).toEqual('78');
+    expect(resourcePanel.find('div[data-testid="resource-gemini1"]').text().includes('O2 Replacement Cells')).toBeTruthy();
+    // gemini 2
+    expect(resourcePanel.find('div[data-testid="energy-gemini2"]').text()).toEqual('40');
+    expect(resourcePanel.find('div[data-testid="lifeSupport-gemini2"]').text()).toEqual('100');
+    expect(resourcePanel.find('div[data-testid="resource-gemini2"]').text()).toEqual('');
+
+    // press next move to HyperGate 2
+    await confirmMove('h2', '38');
+
+    // check resource panel initial resource level
+    gameBoard.update();
+    resourcePanel = gameBoard.find(ResourcePanel);
+    // gemini 1
+    expect(resourcePanel.find('div[data-testid="energy-gemini1"]').text()).toEqual('18');
+    expect(resourcePanel.find('div[data-testid="lifeSupport-gemini1"]').text()).toEqual('73');
     expect(resourcePanel.find('div[data-testid="resource-gemini1"]').text().includes('O2 Replacement Cells')).toBeTruthy();
     // gemini 2
     expect(resourcePanel.find('div[data-testid="energy-gemini2"]').text()).toEqual('40');
